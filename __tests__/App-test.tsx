69578e7530f49214c2e8fae56d73c9506b316455
Ricardo Gomes <ricardo@heptasoft.com>
2020-04-23 21:35:07 +0100

    Removed not used test

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,11 +4,10 @@
 
 import 'react-native';
 import React from 'react';
-import Loading from '../src/screens/Loading';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-	renderer.create(<Loading />);
+	//renderer.create(<Loading />);
 });
